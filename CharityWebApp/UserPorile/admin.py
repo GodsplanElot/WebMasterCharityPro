35410b4a4257e078d86b9ab9308fe3e0ecf1331e
Godsplan Elot <elotgodsplan2@gmail.com>
2025-09-25 01:10:32 +0100

    Fix Bug

--- CharityWebApp/UserPorile/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Profile
-
-# Register your models here.
-@admin.register(Profile)
-class ProfileAdmin(admin.ModelAdmin):
-    list_display = ("user", "verified", "completed", "phone")
-    list_filter = ("verified", "completed", "marital_status")
-    search_fields = ("user__username", "user__email", "phone")
